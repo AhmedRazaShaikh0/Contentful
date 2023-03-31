f6f94179de857a965150dafd30ffbb085640ff03
Ahmed Raza <ahmedraza6100@gmail.com>
2023-03-31 18:49:26 +0500

    api

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    CONTENTFUL_SPACE_ID: 'l231sayctk2k',
+    CONTENTFUL_ACCESS_KEY: '9yjoQZiX2VCSrTyBzI3kuewcQxVvuFKvyl0YK_DDrCk',
+  },
 }
 
 module.exports = nextConfig
